4380de7221ea568d60179d4efee00ddef76f0062
Arthur Roux <arthur.roux@epita.fr>
2022-01-10 19:06:24 +0100

    first steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address } = useWeb3();
+  console.log(`Address: ${address}`);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to the HarryPotterDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect Wallet
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 Wallet connected ! Let's go play some Quidditch 🧹</h1>
     </div>
   );
 };
